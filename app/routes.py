abf23c0aad8604feb21b92c5c638bb938dba0af4
Adam_Gadaczek <adam.gadaczek@wp.pl>
2025-10-18 12:05:51 +0200

    Code refactoring - change create_entry and edit_entry to one: create_or_edit_entry

--- app/routes.py
+++ app/routes.py
@@ -11,33 +11,25 @@ def index():
    return render_template("homepage.html", all_posts=all_posts)
 
 
-@app.route("/new-post/", methods=["GET", "POST"])
-def create_entry():
-   form = EntryForm()
-   errors = None
-   if request.method == 'POST':
-       if form.validate_on_submit():
-           entry = Entry (
-               title=form.title.data,
-               body=form.body.data,
-               is_published=form.is_published.data
-           )
-           db.session.add(entry)
-           db.session.commit()
-       else:
-           errors = form.errors
-   return render_template("entry_form.html", form=form, errors=errors)
-
-
+@app.route("/new-post/", defaults={"entry_id": None}, methods=["GET", "POST"])
 @app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
-def edit_entry(entry_id):
-   entry = Entry.query.filter_by(id=entry_id).first_or_404()
-   form = EntryForm(obj=entry)
-   errors = None
-   if request.method == 'POST':
-       if form.validate_on_submit():
-           form.populate_obj(entry)
-           db.session.commit()
-       else:
-           errors = form.errors
-   return render_template("entry_form.html", form=form, errors=errors)
+def create_or_edit_entry(entry_id):
+    if entry_id is not None:
+        entry = Entry.query.filter_by(id=entry_id).first_or_404()
+    else:
+        entry = None
+  
+    form = EntryForm(obj=entry)
+    errors = None
+
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            if entry is None:
+                entry = Entry()
+                db.session.add(entry)
+            form.populate_obj(entry)
+            db.session.commit()
+        else:
+            errors = form.errors
+
+    return render_template("entry_form.html", form=form, errors=errors)
