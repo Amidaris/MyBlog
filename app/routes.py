7c038181a38d97dcd3af4d47a53fc2e43f1c9453
Adam_Gadaczek <adam.gadaczek@wp.pl>
2025-10-18 11:22:16 +0200

    Add creating new post functionality

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,29 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
-from app.models import Entry
+from app.models import Entry, db
+from app.forms import EntryForm
+
 
 @app.route("/")
 def index():
    all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
 
    return render_template("homepage.html", all_posts=all_posts)
+
+
+@app.route("/new-post/", methods=["GET", "POST"])
+def create_entry():
+   form = EntryForm()
+   errors = None
+   if request.method == 'POST':
+       if form.validate_on_submit():
+           entry = Entry (
+               title=form.title.data,
+               body=form.body.data,
+               is_published=form.is_published.data
+           )
+           db.session.add(entry)
+           db.session.commit()
+       else:
+           errors = form.errors
+   return render_template("entry_form.html", form=form, errors=errors)
