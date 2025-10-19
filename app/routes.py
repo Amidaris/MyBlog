628986a75ce42ae1ebff790faebf0789376a8baa
Adam_Gadaczek <adam.gadaczek@wp.pl>
2025-10-19 09:32:58 +0200

    Add login functionality

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,16 @@
-from flask import render_template, request
+from flask import render_template, request, session, flash, redirect, url_for
 from app import app
 from app.models import Entry, db
-from app.forms import EntryForm
+from app.forms import EntryForm, LoginForm
+import functools
+
+def login_required(view_func):
+   @functools.wraps(view_func)
+   def check_permissions(*args, **kwargs):
+       if session.get('logged_in'):
+           return view_func(*args, **kwargs)
+       return redirect(url_for('login', next=request.path))
+   return check_permissions
 
 
 @app.route("/")
@@ -13,6 +22,7 @@ def index():
 
 @app.route("/new-post/", defaults={"entry_id": None}, methods=["GET", "POST"])
 @app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
+@login_required
 def create_or_edit_entry(entry_id):
     if entry_id is not None:
         entry = Entry.query.filter_by(id=entry_id).first_or_404()
@@ -33,3 +43,27 @@ def create_or_edit_entry(entry_id):
             errors = form.errors
 
     return render_template("entry_form.html", form=form, errors=errors)
+
+
+@app.route("/login/", methods=['GET', 'POST'])
+def login():
+   form = LoginForm()
+   errors = None
+   next_url = request.args.get('next')
+   if request.method == 'POST':
+       if form.validate_on_submit():
+           session['logged_in'] = True
+           session.permanent = True  # Use cookie to store session.
+           flash('You are now logged in.', 'success')
+           return redirect(next_url or url_for('index'))
+       else:
+           errors = form.errors
+   return render_template("login_form.html", form=form, errors=errors)
+
+
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+   if request.method == 'POST':
+       session.clear()
+       flash('You are now logged out.', 'success')
+   return redirect(url_for('index'))
